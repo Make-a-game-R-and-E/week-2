aea3936439394ce564aa4f4da673a1458658bfd7
elyasafko <elyasafko@gmail.com>
2024-11-19 19:28:25 +0200

    a

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,17 +4,21 @@ using UnityEngine.InputSystem;
 public class Interaction : MonoBehaviour
 {
     [SerializeField]
-    private InputAction action = new InputAction("action",type: InputActionType.Button,binding: "<Keyboard>/space");
+    private InputAction action = new InputAction("action", type: InputActionType.Button, binding: "<Keyboard>/space");
 
-    private void OnEnable(){
+    private void OnEnable()
+    {
         action.Enable();
     }
-    private void OnDisable(){
+    private void OnDisable()
+    {
         action.Disable();
     }
 
-    private void Update(){
-        if (action.WasPressedThisFrame()){
+    private void Update()
+    {
+        if (action.WasPressedThisFrame())
+        {
             SpriteRenderer sr = GetComponent<SpriteRenderer>();
             sr.enabled = !sr.enabled;
         }
