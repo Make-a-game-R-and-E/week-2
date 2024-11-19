3faf625e17b5aed0237b9c8da7a331f3d2d3de5d
elyasafko <elyasafko@gmail.com>
2024-11-19 11:13:03 +0200

    fix exit

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -5,9 +5,12 @@ public class ExitGame : MonoBehaviour
     // Function to exit the game
     public void QuitGame()
     {
+#if UNITY_EDITOR
         // Exit Play Mode in the Unity Editor
         UnityEditor.EditorApplication.isPlaying = false;
-        // Close the application
-        Application.Quit();
+#else
+            // Close the application
+            Application.Quit();
+#endif
     }
 }
